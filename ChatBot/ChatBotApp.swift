b042da73be2006dd71b3f98e47ad782fccc2523a
Moussa Noun <moussa@polygoncyber.com>
2026-02-16 11:48:55 -0400

    Initial Commit

---
+++ ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by Moussa Noun on 2026-02-16.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatBot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatBot
+//
+//  Created by Moussa Noun on 2026-02-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
